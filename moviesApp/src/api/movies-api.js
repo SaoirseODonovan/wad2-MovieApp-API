060a768928e90df85c69e05b4e1a18dcc997a52c
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-12-17 20:23:50 +0000

    added to movies-api in the movies app and installed nodemon

--- moviesApp/src/api/movies-api.js
+++ moviesApp/src/api/movies-api.js
@@ -5,3 +5,23 @@ export const getMovies = () => {
         }
     }).then(res => res.json());
 };
+
+export const login = (username, password) => {
+    return fetch('/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
+
+export const signup = (username, password) => {
+    return fetch('/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
